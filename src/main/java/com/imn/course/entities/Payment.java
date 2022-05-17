6979ad355a6ea28dcbff4e2a25fcb590cf31c685
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-05-17 06:50:56 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/imn/course/entities/Payment.java
@@ -0,0 +1,100 @@
+package com.imn.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {
+		
+	}
+
+	public Payment(Long id, Instant moment, Order order) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the moment
+	 */
+	public Instant getMoment() {
+		return moment;
+	}
+
+	/**
+	 * @param moment the moment to set
+	 */
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	/**
+	 * @return the order
+	 */
+	public Order getOrder() {
+		return order;
+	}
+
+	/**
+	 * @param order the order to set
+	 */
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
